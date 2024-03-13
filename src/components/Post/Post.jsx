2f4e6f5005831f898059c737511e7bf3e84d88cb
Mohammad Ashif <Mohammad Ashif>
2024-03-13 17:54:06 +0600

    post info updated

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,10 +2,10 @@ import { CiBookmarkCheck } from "react-icons/ci";
 
 const Post = ({ post }) => {
     console.log(post);
-    const { title, cover_image, author, publish_date, reading_time, tag } =
+    const { title, cover_image, author, publish_date, reading_time, tags } =
         post;
     return (
-        <div className="mb-20">
+        <div className="mb-20 border-2 border-[#000000] p-3 rounded-2xl">
             <img src={cover_image} alt="" />
             <div className="flex justify-between mt-6">
                 <div className="flex items-center gap-3">
@@ -21,6 +21,14 @@ const Post = ({ post }) => {
                 </div>
             </div>
             <p className="text-4xl font-bold my-3">{title}</p>
+            <div className="flex gap-3 my-5">
+                {tags.map((tag, idx) => (
+                    <div key={idx} className="text-[16px] font-bold">
+                        <p>#{tag}</p>
+                    </div>
+                ))}
+            </div>
+            <button className="border-b-2 border-[#000]">Mark as Read</button>
         </div>
     );
 };
